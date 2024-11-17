cf457da51695e19ecfce5f554b66f9480e9fab0f
Nakul Dev <nakuldev1561@gmail.com>
2024-11-17 18:46:43 +0530

    Switched to React

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='bodyx'>
+    <h1>Nakul's 2025 Portfolio Website is Loading...</h1>
+    <p>In the meantime, you can visit the old website:</p><br></br>
+    <a href="https://nakuldevmv.github.io/" target="_blank" rel="noreferrer">Visit Old Portfolio</a>
+    </div>
+  );
+}
+
+export default App;
--- website2025/lib/launch_url.dart
+++
@@ -1,9 +0,0 @@
-import 'package:url_launcher/url_launcher.dart';
-
-urlLaunch(String string) async {
-  if (await canLaunchUrl(Uri.parse(string))) {
-    await launchUrl(Uri.parse(string));
-  } else {
-    throw 'Could not launch $string';
-  }
-}
