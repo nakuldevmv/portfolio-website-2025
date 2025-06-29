d51cc14c12ed3b5cb6299aee4e20f37918b526e8
Nakul Dev <nakuldev1561@gmail.com>
2025-06-29 19:42:58 +0530

    feat: add initial configuration for components and utility functions
    - Created components.json to define project schema, styling, and aliases.
    - Implemented utility function `cn` for merging Tailwind CSS classes using clsx and tailwind-merge.

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,16 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import localFont from "next/font/local";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const degular = localFont({
+  src: './fonts/DegularVariable.woff2',
+  display: 'swap',
+  variable: '--font-degular',
+  weight: '100 900', 
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${degular.variable} antialiased`}
       >
         {children}
       </body>
