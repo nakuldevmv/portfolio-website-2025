0df512a56628a49435dc58a7d0e6791a3534b3db
Nakul Dev <nakuldev1561@gmail.com>
2025-07-27 16:21:51 +0530

    feat: add Vercel Analytics and clean up commented code in layout and toggleTheme components

--- portfolio/app/modules/theme/toggleTheme.tsx
+++ portfolio/app/modules/theme/toggleTheme.tsx
@@ -1,19 +1,19 @@
 
-import { useTheme } from "next-themes";
-import style from "./toggleTheme.module.css"
+// import { useTheme } from "next-themes";
+// import style from "./toggleTheme.module.css"
 import ThemeToggleButton from "@/components/ui/theme-toggle-button";
 
 export default function ToggleTheme() {
-    const { theme, setTheme } = useTheme();
+    // const { theme, setTheme } = useTheme();
 
     return (
         <>
         <ThemeToggleButton start="top-right"/>
 
 
-        <button className={style.btn} onClick={() => { setTheme(theme == "light" ? "dark" : "light") }}>
+        {/* <button className={style.btn} onClick={() => { setTheme(theme == "light" ? "dark" : "light") }}>
             {theme == "light" ? "Dark Mode" : "Light Mode"}
-        </button>
+        </button> */}
         
         </>
 
