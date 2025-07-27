29e07c4cb3e1b8a4a4f22b3c6578b8c634519997
Nakul Dev <nakuldev1561@gmail.com>
2025-07-27 19:21:57 +0530

    feat: add AboutMe component with styling and content

--- portfolio/app/page.jsx
+++ portfolio/app/page.jsx
@@ -4,15 +4,17 @@ import Projects from "./modules/userDefined/projects/projects";
 import Skills from "./modules/userDefined/skills/skills"
 import NavBar from "./modules/userDefined/navbar/navbar";
 import Landing from "./modules/userDefined/landing/landing";
+import AboutMe from "./modules/userDefined/aboutme/aboutme";
 
 export default function Home() {
   return (
     <>
       {/* <NavBar /> */}
       <Landing />
+      <AboutMe />
       <ToggleTheme />
-      <Skills />
-      <Projects />
+      {/* <Skills />
+      <Projects /> */}
     </>
   );
 }
