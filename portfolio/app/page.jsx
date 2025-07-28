b1d94c5708f4b8acc9b9cf0b88e2d1943017a054
Nakul Dev <nakuldev1561@gmail.com>
2025-07-28 21:36:07 +0530

    feat: implement client-side rendering with mounted state management

--- portfolio/app/page.jsx
+++ portfolio/app/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect, useState } from "react";
 import ToggleTheme from "./modules/theme/toggleTheme";
 import Projects from "./modules/userDefined/projects/projects";
 import Skills from "./modules/userDefined/skills/skills"
@@ -7,11 +8,18 @@ import Landing from "./modules/userDefined/landing/landing";
 import AboutMe from "./modules/userDefined/aboutme/aboutme";
 
 export default function Home() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null; // Avoid SSR mismatch
   return (
     <>
       {/* <NavBar /> */}
       <Landing />
-      <ToggleTheme />
+      {/* <ToggleTheme /> */}
       <AboutMe />
       <Skills />
       <Projects />
