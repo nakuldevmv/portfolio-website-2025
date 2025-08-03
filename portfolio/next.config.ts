b99d576a91335ad556662c824bf3304ee6e5245b
Nakul Dev <nakuldev1561@gmail.com>
2025-08-03 18:52:24 +0530

    Fix indentation in webpack configuration for SVG handling

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -12,13 +12,13 @@ const nextConfig: NextConfig = {
         },
     },
     webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
+        config.module.rules.push({
+            test: /\.svg$/,
+            issuer: /\.[jt]sx?$/,
+            use: ['@svgr/webpack'],
+        });
+        return config;
+    },
 
 };
 
