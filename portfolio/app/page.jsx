ef8b09ed7580112f860c37f5ff7ca6fd08c02686
Nakul Dev <nakuldev1561@gmail.com>
2025-08-09 21:56:06 +0530

    off

--- portfolio/app/page.jsx
+++ portfolio/app/page.jsx
@@ -19,14 +19,14 @@ export default function Home() {
 
   return (
     <>
-      <NavBar />
+      {/* <NavBar /> */}
       
-      <div id="home"><Landing /></div>
+      {/* <div id="home"><Landing /></div> */}
       {/* BELOW-FOLD COMPONENTS */}
       <div className={`transition-opacity duration-300 ease-in ${showRest ? 'opacity-100' : 'opacity-0'}`}>
-        <div id="about"><AboutMe /></div>
+        {/* <div id="about"><AboutMe /></div> */}
         
-        <div id="project"><Projects /></div>
+        {/* <div id="project"><Projects /></div> */}
         {/* <div id="skill"><Skills /></div> */}
       </div>
       
