9d261045cdf7fc9e8049f39fd8f263d365304a41
Nakul Dev <nakuldev1561@gmail.com>
2025-08-19 13:55:57 +0530

    added og-image

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -16,8 +16,40 @@ const degular = localFont({
 })
 
 export const metadata: Metadata = {
-  title: "Nakul Dev",
-  description: "Hey, Nakul here!",
+  metadataBase: new URL("https://nakuldev.vercel.app"),
+  title: {
+    default: "Nakul Dev",
+    template: "%s | Nakul Dev",
+  },
+  description:
+    "Portfolio of Nakul Dev - a creative developer building sleek, minimal, and modern web experiences with Next.js, React, and cutting-edge tools.",
+  keywords: [
+    "Nakul Dev",
+    "Nakul Dev M V",
+    "Creative Developer",
+    "Developer Portfolio",
+    "Next.js Developer",
+    "Frontend Engineer",
+    "Web Developer Portfolio",
+  ],
+  authors: [{ name: "Nakul Dev", url: "https://nakuldev.vercel.app" }],
+  openGraph: {
+    title: "Nakul Dev | Creative Developer Portfolio",
+    description:
+      "Discover the portfolio of Nakul Dev - creative developer showcasing modern web projects, design-driven builds, and unique digital experiences.",
+    url: "https://nakuldev.vercel.app",
+    siteName: "Nakul Dev",
+    images: [
+      {
+        url: "/og-image.png", // 1200x630
+        width: 1200,
+        height: 630,
+        alt: "Preview of Nakul Dev Portfolio",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
   icons: {
     icon: [
       { url: "/favicon.png", sizes: "32x32", type: "image/png" },
@@ -26,8 +58,7 @@ export const metadata: Metadata = {
     apple: "/favicon.png",
   },
   manifest: "/site.webmanifest",
-};
-
+}
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -35,6 +66,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: `
+{
+  "@context": "https://schema.org",
+  "@type": "Person",
+  "name": "Nakul Dev",
+  "alternateName": "Nakul Dev M V",
+  "url": "https://nakuldev.vercel.app/",
+  "email": "mailto:nakuldevmv@gmail.com",
+  "sameAs": [
+    "https://www.linkedin.com/in/nakuldevmv/",
+    "https://github.com/nakuldevmv",
+    "https://instagram.com/nakuled"
+  ]
+}
+` }}
+        />
+
+      </head>
       <body
         className={`${degular.variable} antialiased`}
       >
@@ -51,6 +104,7 @@ export default function RootLayout({
         </ThemeProvider>
 
       </body>
+
     </html>
   );
 }
